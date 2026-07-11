c9d41e39f8a67219678db557b2ab875cd1592824
Alug <alugfolie@proton.me>
2026-07-11 04:21:58 +0200

    extern c qs22j
    so it can be used in ceepeepee code

--- prboom2/src/qs22j.h
+++ prboom2/src/qs22j.h
@@ -1,7 +1,14 @@
 #ifndef __QS22J__
 #define __QS22J__
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void qs22j(void *base, size_t nmemb, size_t size,
                                      int (*compar)(const void *, const void *));
+#ifdef __cplusplus
+} // extern "C"
+#endif
 
 #endif
